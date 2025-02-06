7e448c2e9f651d0772b410b172175933161e39a9
CVTenorio <202310471@fit.edu.ph>
2025-02-06 21:36:09 +0800

    program 2 done

--- TenorioP2.py
+++ TenorioP2.py
@@ -1,3 +1,15 @@
 #Program 2
 
-print("Hello World")
+def SumOfDigits (string):
+    temp = "0"
+    total = 0
+    for char in string:
+        if (char.isdigit()):
+            temp += char
+        else:
+            total += int(temp)
+            temp = "0"
+    return total + int(temp)
+
+Word = input ("Enter a String: ")
+print ("Sum of digits from string: ", SumOfDigits(Word))
